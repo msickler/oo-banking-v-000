fb54cea533b4d18f6139dfe1e63eb76f8e3364d0
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-01 14:13:16 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,8 +1,12 @@
 class Transfer
- attr_accessor :status
+ attr_accessor :status, :sender, :receiver, :amount
 
  def initialize(status = "pending")
    @status = status
+   @sender = sender
+   @receiver = receiver 
+   @amount = amount
+   
  end
 
 end
